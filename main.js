905400aa60f4ba72d11a80b7b521f603f32117cc
Phoenixfusion <79008006+whitatkkrshreash@users.noreply.github.com>
2022-03-06 17:31:07 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 img="";
 status="";
+objects= [];
 function preload(){
     img =loadImage("dog_cat.jpg");
 }
@@ -23,14 +24,15 @@ function gotResult(error,results){
 function draw(){
     //dog
     image(img,0,0,640,420);
-    text("Dog",45,75);
-    noFill();
-    stroke("#FF0000");
-    rect(30,60,450,350);
-
-    //Cat
-    text("Cat",320,120);
-    noFill();
-    stroke("#FF0000");
-    rect(300,90,270,320);
+    if(status !=""){
+        for (i=0;i<objects.length;i++){
+            document.getElementById("status").innerHTML="Status: Detected Object";
+            Fill("red");
+            percent= floor(objects[i].confidence*100);
+            text(objects[i].label+""+percent+"%"+ objects[i].x+15, objects[i].y+15);
+            nofill();
+            stroke("red");
+            rect(objects[i].x, object[i].y, objects[i].width, objects[i].height);
+        }
+    }
 }
